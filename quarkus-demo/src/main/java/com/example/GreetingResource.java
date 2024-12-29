376bb7dc02b625ed0f919395c9e2922b0eb122bf
roshinivr <{ID}+{username}@users.noreply.github.com>
2024-12-29 21:25:15 +0000

    Add Swagger api documentation

--- quarkus-demo/src/main/java/com/example/GreetingResource.java
+++ quarkus-demo/src/main/java/com/example/GreetingResource.java
@@ -5,7 +5,10 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 @Path("/api")
+@Tag(name = "Hello Resource", description = "Example resource to demonstrate Swagger setup")
 public class GreetingResource {
 
     @GET
